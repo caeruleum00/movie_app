b1de6993ee033848304321fede7224202b74e3b2
caeruleum00 <caeruleum00@naver.com>
2019-10-10 19:51:39 +0900

    Updates

---
+++ precache-manifest.e3e1b41d7fc7265760c541b5ee5a9fe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f5824ffbb0a877e9b4ac265e213321",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "2b66d517e249c6dc88bd",
+    "url": "/movie_app/static/css/main.55cd9d53.chunk.css"
+  },
+  {
+    "revision": "f2882efa174f6e045654",
+    "url": "/movie_app/static/js/2.acbe3922.chunk.js"
+  },
+  {
+    "revision": "2b66d517e249c6dc88bd",
+    "url": "/movie_app/static/js/main.00a970eb.chunk.js"
+  },
+  {
+    "revision": "f9c5d4d9cea5f935b75a",
+    "url": "/movie_app/static/js/runtime~main.b05fbdd8.js"
+  }
+]);
